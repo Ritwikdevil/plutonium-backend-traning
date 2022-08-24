ff856acca624a60459e6d7098687f409cf99c413
ritwik <ritwik6927@gmail.com>
2022-08-24 22:57:35 +0530

    global middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,7 +15,22 @@ mongoose.connect("mongodb+srv://ritwikmukherjee:OD2EVF8X47p26tdw@cluster0.tzjat9
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route);
+
+
+//Global middleware
+app.use(
+    function globalMw(req, res, next) {
+        const today = moment();
+        let formatted = today.format('YYYY-MM-DD HH:MM:SS' );
+        console.log(formatted, ",", req.ip, ",", req.originalUrl);
+        next()
+    } 
+
+)
+
+
+ app.use('/', route);
+
 
 
 app.listen(process.env.PORT || 3000, function () {
